60dbe8b588315d62e72fe9525a624ba6ab1e7919
khan019950 <fymcabatcha2016@gmail.com>
2018-10-05 17:07:10 +0530

    Initial Commit

---
+++ ed-aid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ed-aid
+//
+//  Created by abdul khan on 05/10/18.
+//  Copyright © 2018 abdul khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
